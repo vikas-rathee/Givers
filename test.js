814f03f1a3b9365291df9afd4f6084ecd89915cc
Rohit Kumar <33492424+Rohit-Kumarji@users.noreply.github.com>
2018-10-20 19:08:18 +0530

    delete the test.js

--- test.js
+++ test.js
@@ -1,19 +1,1 @@
-var obj = function(){
-  console.log("function");
-  return 1;
-};
 
-obj.fun = function(){
-  console.log("Fun");
-  return 2;
-};
-
-var count = 0;
-
-for(var prop in obj){
-  console.log(prop);
-  count++;
-}
-console.log(count);
-// console.log(obj());
-// console.log(obj.fun());
